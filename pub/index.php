961480104e66e02433c384ec64cc911815342962
Benjamin Cartagena <student@ws08-2.local>
2012-09-06 11:19:08 -0400

    first commit

---
+++ pub/index.php
@@ -0,0 +1,6 @@
+<h3> Vida Guerra is amazing</h3>
+<br>
+echo "Integrating this line as well";
+<br><br>
+<img src="http://queensofkings.com/wp-content/uploads/2012/05/vida-guerra-50.jpg"/> 
+<br><br>
